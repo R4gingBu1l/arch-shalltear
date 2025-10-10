288727623b46d464fd59b0e5d0a620ab27aa2568
sane1090x <sane1090x@proton.me>
2025-10-10 16:23:02 +0530

    Update and clean up Rofi config

--- .config/rofi/launchers/type-6/launcher.sh
+++ .config/rofi/launchers/type-6/launcher.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-## Author : Aditya Shakya (adi1090x)
-## Github : @adi1090x
-#
-## Rofi   : Launcher (Modi Drun, Run, File Browser, Window)
-#
 ## Available Styles
 #
 ## style-1     style-2     style-3     style-4     style-5
